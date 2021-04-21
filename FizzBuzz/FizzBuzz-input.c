f8f44f4e2460718126e9be09038c998b1900f285
Isabella <Bean6754@users.noreply.github.com>
2021-04-21 16:29:23 +0100

    Update FizzBuzz-input.c
    Cleaner code.

--- FizzBuzz/FizzBuzz-input.c
+++ FizzBuzz/FizzBuzz-input.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	int i;
-	int input;
-	// int i = 1 is not needed as the for loop will set i as 1 initially.
-	
-	printf("How many do you want to count? ");	
-	scanf("%d", &input);
-	
-	for(i = 1; i < input; i = i + 1){
-		//printf("Value of i: %d\n", i);
-		
-		if( i % 3 == 0 && i % 5 == 0 ) {
-			printf("FizzBuzz.\n");
-		}
-		else if( i % 3 == 0 ) {
-			printf("Fizz.\n");
-		}
-		else if( i % 5 == 0 ) {
-			printf("Buzz.\n");
-		}
-		else {
-			printf("%d\n", i + 1);
-		}
-	}	
-	
-	return 0;
+int main(void)
+{
+  int i;
+  int input;
+  // int i = 1 is not needed as the for loop will set i as 1 initially.
+
+  printf("How many do you want to count? ");
+  scanf("%d", &input);
+
+  for(i = 1; i < input; i = i + 1)
+  {
+    //printf("Value of i: %d\n", i);
+
+    if( i % 3 == 0 && i % 5 == 0 )
+    {
+      printf("FizzBuzz.\n");
+    }
+    else if( i % 3 == 0 )
+    {
+      printf("Fizz.\n");
+    }
+    else if( i % 5 == 0 )
+    {
+      printf("Buzz.\n");
+    }
+    else
+    {
+      printf("%d\n", i + 1);
+    }
+  }
+
+  return 0;
 }
+
