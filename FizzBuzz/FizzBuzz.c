ba473b75b3751dd996b15eac6d35b8bcd15959b5
Isabella <Bean6754@users.noreply.github.com>
2021-04-21 16:32:15 +0100

    Update FizzBuzz.c
    Cleaner code.

--- FizzBuzz/FizzBuzz.c
+++ FizzBuzz/FizzBuzz.c
@@ -3,25 +3,29 @@
 
 int main(void)
 {
-	int i;
-	// int i = 1 is not needed as the for loop will set i as 1 initially.
-	
-	for( i = 1; i < 101; i = i + 1 ){
-		//printf("Value of i: %d\n", i);
-		
-		if( i % 3 == 0 && i % 5 == 0 ) {
-			printf("FizzBuzz.\n");
-		}
-		else if( i % 3 == 0 ) {
-			printf("Fizz.\n");
-		}
-		else if( i % 5 == 0 ) {
-			printf("Buzz.\n");
-		}
-		else {
-			printf("%d\n", i);
-		}
-	}	
-	
-	return 0;
+  int i;
+  // int i = 1 is not needed as the for loop will set i as 1 initially.
+
+  for( i = 1; i < 101; i = i + 1 )
+  {
+    //printf("Value of i: %d\n", i);
+    if( i % 3 == 0 && i % 5 == 0 )
+    {
+      printf("FizzBuzz.\n");
+    }
+    else if( i % 3 == 0 )
+    {
+      printf("Fizz.\n");
+    }
+    else if( i % 5 == 0 )
+    {
+      printf("Buzz.\n");
+    }
+    else
+    {
+      printf("%d\n", i);
+    }
+  }
+
+  return 0;
 }
