8e7003bb5585f9856fad42531ea6b139e31383e8
Isabella <Bean6754@users.noreply.github.com>
2022-06-21 14:44:36 +0100

    Update FizzBuzz-input.c

--- FizzBuzz/FizzBuzz-input.c
+++ FizzBuzz/FizzBuzz-input.c
@@ -10,19 +10,19 @@ int main(void)
   printf("How many do you want to count? ");
   scanf("%d", &input);
 
-  for(i = 1; i < (input + 1); i = i + 1)
+  for (i = 1; i < (input + 1); i = i + 1)
   {
     //printf("Value of i: %d\n", i);
 
-    if( i % 3 == 0 && i % 5 == 0 )
+    if (i % 3 == 0 && i % 5 == 0)
     {
       printf("FizzBuzz.\n");
     }
-    else if( i % 3 == 0 )
+    else if (i % 3 == 0)
     {
       printf("Fizz.\n");
     }
-    else if( i % 5 == 0 )
+    else if (i % 5 == 0)
     {
       printf("Buzz.\n");
     }
